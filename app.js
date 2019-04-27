6b393de53713d77996d0fbb5339d466de3faf96b
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-04-26 18:26:04 -0700

    Made a request from the Youtube API, made the video dynamically create on the page

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-console.log("hellooooooo");
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+var videoId;
+var videoQuery = "lime chicken";
+var queryURL =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
+  videoQuery +
+  "+recipe&type=video&key=AIzaSyAaJCFnN_yhqsDaDZY5V2QhoFQnExxckRI";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+  videoId = response.items[2].id.videoId;
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+});
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("player", {
+    height: "390",
+    width: "640",
+    videoId: videoId,
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
+}
