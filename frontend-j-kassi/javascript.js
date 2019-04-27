cab026ab0279c5a48ab54c73c91d87d0487ad0d3
thepickupkitten <jkashmoon@gmail.com>
2019-04-27 11:40:55 -0700

    basic structure and files commit

---
+++ frontend-j-kassi/javascript.js
@@ -0,0 +1,18 @@
+$("#tile-button").on("click", function {
+    // switch the result-div width and the contents within it
+    
+    // var resultItem = $("<div>");
+    // resultItem.addClass("result-div");
+    // resultItem.attr("width", "100px");
+    // $("#multi-results").append(resultItem);
+    // switch the attribute class "listview"
+    $("result-div").attr("list-view", "false");
+    $("result-div").attr("width", "100px");
+
+});
+
+$("#list-button").on("click", function {
+    // switch the result-div width and the contents within it
+    $("result-div").attr("list-view", "true");
+    $("result-div").attr("width", "100%");
+});
